7d9f54efd53c949b2036266e7759bb41fd86ce4e
Aya Farag <ayasaid94.as@gmail.com>
2025-10-22 23:06:22 +0400

    product example task

--- src/Componants/home-componant/home-componant.ts
+++ src/Componants/home-componant/home-componant.ts
@@ -1,5 +1,6 @@
 import { CommonModule  } from '@angular/common';
 import { Component } from '@angular/core';
+import { IProduct } from '../../Models/iproduct';
 
 @Component({
   selector: 'app-home-componant',
@@ -16,6 +17,19 @@ export class HomeComponant {
   id: number = 1;
 
   skills!: string[] ;
+
+  products: IProduct[] = [
+    { title: "Laptop" , description: "test out laptop" , price: 100, quantity: 10},
+    { title: "Laptop" , description: "test out laptop" , price: 100, quantity: 10},
+    { title: "Laptop" , description: "test out laptop" , price: 100, quantity: 10},
+    { title: "Laptop" , description: "test out laptop" , price: 100, quantity: 10},
+  ];
+
+  productDetails : IProduct = { title:" " , description: "" , price: 100 , quantity: 20}; 
+
+
+  productTitle: string = "Labtop";
+  
 
   buttonClicked(){
     console.log("test button clicked");
